8c3a34e5e5ce66432df359c87f65ce8f1bb32fc6
Nixx <nixxklein@gmail.com>
2024-01-31 01:00:08 +0200

    Initialize interactive-code-6693

---
+++ packages/web3-core/test/unit/web3_subscription.test.ts
@@ -0,0 +1,184 @@
+﻿/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { Web3SubscriptionManager } from '../../src';
+import { ExampleSubscription } from './fixtures/example_subscription';
+
+const subscriptions = { example: ExampleSubscription as never };
+
+describe('Web3Subscription', () => {
+	let requestManager: any;
+	let subscriptionManager: Web3SubscriptionManager<any, any>;
+	let sub: ExampleSubscription;
+
+	beforeEach(() => {
+		requestManager = {
+			send: jest.fn().mockImplementation(async () => {
+				return 'sub-id';
+			}),
+			on: jest.fn(),
+			provider: { on: jest.fn(), removeListener: jest.fn(), request: jest.fn() },
+		};
+		subscriptionManager = new Web3SubscriptionManager(requestManager, subscriptions);
+	});
+
+	describe('subscriptionManager', () => {
+		it('subscriptionManager is accessible in inherited subscription', async () => {
+			class InheritedExampleSubscription extends ExampleSubscription {
+				public verifyAccessToSubscriptionManager(
+					originalSubscriptionManager: Web3SubscriptionManager,
+				) {
+					expect(this.subscriptionManager).toBe(originalSubscriptionManager);
+				}
+			}
+			new InheritedExampleSubscription(
+				{ param1: 'value' },
+				{ subscriptionManager },
+			).verifyAccessToSubscriptionManager(subscriptionManager);
+		});
+	});
+
+	describe('subscribe', () => {
+		beforeEach(() => {
+			sub = new ExampleSubscription({ param1: 'value' }, { subscriptionManager });
+		});
+		it('should invoke request manager for subscription', async () => {
+			await sub.subscribe();
+
+			expect(requestManager.send).toHaveBeenCalledTimes(1);
+			expect(requestManager.send).toHaveBeenCalledWith({
+				method: 'eth_subscribe',
+				params: ['newHeads'],
+			});
+		});
+
+		it('should set correct subscription id', async () => {
+			expect(sub.id).toBeUndefined();
+			await sub.subscribe();
+			expect(sub.id).toBe('sub-id');
+		});
+
+		it('should start listening to the "message" event', async () => {
+			await sub.subscribe();
+
+			expect(requestManager.provider.on).toHaveBeenCalledTimes(1);
+			expect(requestManager.provider.on).toHaveBeenCalledWith(
+				'message',
+				expect.any(Function),
+			);
+		});
+	});
+
+	describe('unsubscribe', () => {
+		beforeEach(() => {
+			sub = new ExampleSubscription({ param1: 'value' }, { subscriptionManager });
+			sub['_id'] = 'sub-id';
+			subscriptionManager.subscriptions.set('sub-id', sub);
+		});
+
+		it('should invoke request manager to unsubscribe', async () => {
+			await sub.unsubscribe();
+
+			expect(requestManager.send).toHaveBeenCalledTimes(1);
+			expect(requestManager.send).toHaveBeenCalledWith({
+				method: 'eth_unsubscribe',
+				params: ['sub-id'],
+			});
+		});
+
+		it('should remove the subscription id', async () => {
+			expect(sub.id).toBe('sub-id');
+			await sub.unsubscribe();
+			expect(sub.id).toBeUndefined();
+		});
+	});
+});
+
+describe('Web3Subscription without subscription manager - (deprecated)', () => {
+	let requestManager: any;
+	let sub: ExampleSubscription;
+
+	beforeEach(() => {
+		requestManager = {
+			send: jest.fn().mockImplementation(async () => {
+				return 'sub-id';
+			}),
+			on: jest.fn(),
+			provider: { on: jest.fn(), removeListener: jest.fn(), request: jest.fn() },
+		};
+	});
+	describe('subscribe', () => {
+		beforeEach(() => {
+			// eslint-disable-next-line deprecation/deprecation
+			sub = new ExampleSubscription({ param1: 'value' }, { requestManager });
+		});
+
+		it('should invoke request manager for subscription', async () => {
+			(requestManager.send as jest.Mock).mockResolvedValue('sub-id');
+			await sub.subscribe();
+
+			expect(requestManager.send).toHaveBeenCalledTimes(1);
+			expect(requestManager.send).toHaveBeenCalledWith({
+				method: 'eth_subscribe',
+				params: ['newHeads'],
+			});
+		});
+
+		it('should set correct subscription id', async () => {
+			(requestManager.send as jest.Mock).mockResolvedValue('sub-id');
+
+			expect(sub.id).toBeUndefined();
+			await sub.subscribe();
+			expect(sub.id).toBe('sub-id');
+		});
+
+		it('should start listening to the "message" event', async () => {
+			// requestManager.provider.on.mockClear();
+			await sub.subscribe();
+
+			expect(requestManager.provider.on).toHaveBeenCalledTimes(1);
+			expect(requestManager.provider.on).toHaveBeenCalledWith(
+				'message',
+				expect.any(Function),
+			);
+		});
+	});
+
+	describe('unsubscribe', () => {
+		beforeEach(() => {
+			// eslint-disable-next-line deprecation/deprecation
+			sub = new ExampleSubscription({ param1: 'value' }, { requestManager });
+			sub['_id'] = 'sub-id';
+		});
+
+		it('should invoke request manager to unsubscribe', async () => {
+			await sub.unsubscribe();
+
+			expect(requestManager.provider.on).toHaveBeenCalledTimes(1);
+			expect(requestManager.provider.on).toHaveBeenCalledWith(
+				'message',
+				expect.any(Function),
+			);
+		});
+
+		it('should remove the subscription id', async () => {
+			expect(sub.id).toBe('sub-id');
+			await sub.unsubscribe();
+			expect(sub.id).toBeUndefined();
+		});
+	});
+});
---
+++ packages/web3-core/test/unit/web3_subscription_old_providers.test.ts
@@ -0,0 +1,124 @@
+﻿/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { ExampleSubscription } from './fixtures/example_subscription';
+import { Web3EventEmitter } from '../../src/web3_event_emitter';
+import { Web3SubscriptionManager } from '../../src';
+
+describe('Web3Subscription', () => {
+	let requestManager: any;
+	let subscriptionManager: Web3SubscriptionManager<any, any>;
+	let eipRequestManager: any;
+	let subscriptionManagerWithEipReqMan: Web3SubscriptionManager<any, any>;
+	let provider: Web3EventEmitter<any>;
+	let eipProvider: Web3EventEmitter<any>;
+
+	beforeEach(() => {
+		provider = new Web3EventEmitter();
+		eipProvider = new Web3EventEmitter();
+		// @ts-expect-error add to test eip providers
+		eipProvider.request = jest.fn();
+		requestManager = {
+			send: jest.fn().mockImplementation(async () => {
+				return 'sub-id';
+			}),
+			on: jest.fn(),
+			provider,
+		};
+		subscriptionManager = new Web3SubscriptionManager(requestManager, {});
+
+		eipRequestManager = {
+			send: jest.fn().mockImplementation(async () => {
+				return 'sub-id';
+			}),
+			on: jest.fn(),
+			provider: eipProvider,
+		};
+		subscriptionManagerWithEipReqMan = new Web3SubscriptionManager(eipRequestManager, {});
+	});
+
+	describe('providers response for old provider', () => {
+		it('data with result', async () => {
+			const sub = new ExampleSubscription({ param1: 'param1' }, { subscriptionManager });
+			await sub.subscribe();
+			const testData = {
+				data: {
+					subscription: sub.id,
+					result: {
+						some: 1,
+					},
+				},
+			};
+			const processResult = jest.spyOn(sub, '_processSubscriptionResult');
+			provider.emit('data', testData);
+			expect(processResult).toHaveBeenCalledWith(testData.data.result);
+		});
+
+		it('data without result for old provider', async () => {
+			const sub = new ExampleSubscription({ param1: 'param1' }, { subscriptionManager });
+			await sub.subscribe();
+			const testData = {
+				data: {
+					subscription: sub.id,
+					other: {
+						some: 1,
+					},
+				},
+			};
+			const processResult = jest.spyOn(sub, '_processSubscriptionResult');
+			provider.emit('data', testData);
+			expect(processResult).toHaveBeenCalledWith(testData.data);
+		});
+		it('data with result for eipProvider', async () => {
+			const sub = new ExampleSubscription(
+				{ param1: 'param1' },
+				{ subscriptionManager: subscriptionManagerWithEipReqMan },
+			);
+			await sub.subscribe();
+			const testData = {
+				data: {
+					subscription: sub.id,
+					result: {
+						some: 1,
+					},
+				},
+			};
+			const processResult = jest.spyOn(sub, '_processSubscriptionResult');
+			eipProvider.emit('message', testData);
+			expect(processResult).toHaveBeenCalledWith(testData.data.result);
+		});
+
+		it('data without result for eipProvider', async () => {
+			const sub = new ExampleSubscription(
+				{ param1: 'param1' },
+				{ subscriptionManager: subscriptionManagerWithEipReqMan },
+			);
+			await sub.subscribe();
+			const testData = {
+				data: {
+					subscription: sub.id,
+					other: {
+						some: 1,
+					},
+				},
+			};
+			const processResult = jest.spyOn(sub, '_processSubscriptionResult');
+			eipProvider.emit('message', testData);
+			expect(processResult).toHaveBeenCalledWith(testData.data);
+		});
+	});
+});
