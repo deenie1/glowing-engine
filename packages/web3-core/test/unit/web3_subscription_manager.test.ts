3ba838bf44855c7490e4717233725f0050dd1337
Nixx <nixxklein@gmail.com>
2024-01-31 00:59:05 +0200

    Initialize 6485-throw-insecurecontexterror-if-window.crypto.subtle-is-undefined

---
+++ packages/web3-core/test/unit/web3_subscription_manager.test.ts
@@ -0,0 +1,224 @@
+﻿/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { DEFAULT_RETURN_FORMAT } from 'web3-types';
+import { Web3RequestManagerEvent } from '../../src/web3_request_manager';
+import { Web3SubscriptionManager } from '../../src/web3_subscription_manager';
+import { ExampleSubscription } from './fixtures/example_subscription';
+
+jest.mock('./fixtures/example_subscription');
+
+const subscriptions = { example: ExampleSubscription as never };
+
+describe('Web3SubscriptionManager', () => {
+	let requestManager: any;
+	let subManager: Web3SubscriptionManager<any, any>;
+
+	beforeEach(() => {
+		requestManager = {
+			send: jest.fn().mockImplementation(async () => {
+				return 'sub-id';
+			}),
+			on: jest.fn(),
+			provider: { on: jest.fn() },
+		};
+		subManager = new Web3SubscriptionManager(requestManager, subscriptions);
+		(ExampleSubscription as jest.Mock).mockClear();
+	});
+
+	describe('constructor', () => {
+		it('should create subscription manager object', () => {
+			subManager = new Web3SubscriptionManager(requestManager, {});
+			expect(subManager).toBeInstanceOf(Web3SubscriptionManager);
+		});
+
+		it('should create register events for request manager', () => {
+			const requestMan: any = {
+				send: jest.fn(),
+				on: jest.fn(),
+				provider: {
+					on: jest
+						.fn()
+						.mockImplementation((_: string, callback: (a: string) => unknown) =>
+							callback('something'),
+						),
+				},
+			};
+			const subscriptionMan = new Web3SubscriptionManager(requestMan, {});
+
+			expect(subscriptionMan).toBeDefined();
+			expect(requestMan.on).toHaveBeenCalledTimes(2);
+			expect(requestMan.on).toHaveBeenCalledWith(
+				Web3RequestManagerEvent.BEFORE_PROVIDER_CHANGE,
+				expect.any(Function),
+			);
+			expect(requestMan.on).toHaveBeenCalledWith(
+				Web3RequestManagerEvent.PROVIDER_CHANGED,
+				expect.any(Function),
+			);
+		});
+
+		it('should register the subscription types', () => {
+			subManager = new Web3SubscriptionManager(requestManager, {
+				example: ExampleSubscription as never,
+			});
+
+			expect(subManager.registeredSubscriptions).toEqual(subscriptions);
+		});
+	});
+
+	describe('subscribe', () => {
+		beforeEach(() => {
+			subManager = new Web3SubscriptionManager(requestManager, subscriptions);
+
+			jest.spyOn(subManager, 'supportsSubscriptions').mockReturnValue(true);
+		});
+
+		it('should throw error if current provider not available', async () => {
+			delete requestManager.provider;
+			jest.spyOn(subManager, 'supportsSubscriptions').mockReturnValue(false);
+
+			await expect(subManager.subscribe('example')).rejects.toThrow('Provider not available');
+		});
+
+		it('should throw error if subscription is not supported', async () => {
+			jest.spyOn(subManager, 'supportsSubscriptions').mockReturnValue(false);
+
+			await expect(subManager.subscribe('example')).rejects.toThrow(
+				'The current provider does not support subscriptions',
+			);
+		});
+
+		it('should throw error if invalid subscription type is called', async () => {
+			await expect(subManager.subscribe('example2')).rejects.toThrow(
+				'Invalid subscription type',
+			);
+		});
+
+		it('should return valid subscription type if subscribed', async () => {
+			jest.spyOn(subManager, 'addSubscription').mockResolvedValue('123');
+			const result = await subManager.subscribe('example');
+
+			expect(result).toBeInstanceOf(ExampleSubscription);
+		});
+
+		it('should initialize subscription with valid args', async () => {
+			jest.spyOn(subManager, 'addSubscription').mockResolvedValue('456');
+			const result = await subManager.subscribe('example', { test1: 'test1' });
+
+			expect(result).toBeInstanceOf(ExampleSubscription);
+			expect(ExampleSubscription).toHaveBeenCalledTimes(1);
+			expect(ExampleSubscription).toHaveBeenCalledWith(
+				{ test1: 'test1' },
+				{ subscriptionManager: subManager, returnFormat: DEFAULT_RETURN_FORMAT },
+			);
+		});
+	});
+
+	describe('addSubscription', () => {
+		let sub: ExampleSubscription;
+
+		beforeEach(() => {
+			subManager = new Web3SubscriptionManager(requestManager, subscriptions);
+			jest.spyOn(subManager, 'supportsSubscriptions').mockReturnValue(true);
+			sub = new ExampleSubscription(
+				{ param1: 'param1' },
+				{ subscriptionManager: subManager },
+			);
+
+			(sub as any).id = '123';
+		});
+
+		it('should throw error if a subscription already exists with same id', async () => {
+			await subManager.addSubscription(sub);
+
+			await expect(subManager.addSubscription(sub)).rejects.toThrow(
+				'Subscription with id "123" already exists',
+			);
+		});
+
+		it('should try to subscribe the subscription', async () => {
+			sub = new ExampleSubscription(
+				{ param1: 'param1' },
+				{ subscriptionManager: subManager },
+			);
+			jest.spyOn(sub, 'sendSubscriptionRequest').mockImplementation(async () => {
+				(sub as any).id = 'value';
+				return Promise.resolve(sub.id as string);
+			});
+			await subManager.addSubscription(sub);
+
+			expect(sub.sendSubscriptionRequest).toHaveBeenCalledTimes(1);
+			expect(sub.sendSubscriptionRequest).toHaveBeenCalledWith();
+		});
+
+		it('should set the subscription to the map', async () => {
+			expect(subManager.subscriptions).toEqual(new Map());
+
+			await subManager.addSubscription(sub);
+
+			expect(subManager.subscriptions).toEqual(new Map([['123', sub]]));
+		});
+	});
+
+	describe('removeSubscription', () => {
+		let sub: ExampleSubscription;
+
+		beforeEach(async () => {
+			subManager = new Web3SubscriptionManager(requestManager, subscriptions);
+			jest.spyOn(subManager, 'supportsSubscriptions').mockReturnValue(true);
+			sub = new ExampleSubscription(
+				{ param1: 'param1' },
+				{ subscriptionManager: subManager },
+			);
+
+			(sub as any).id = '123';
+			await subManager.addSubscription(sub);
+		});
+
+		it('should throw error if a subscription id does not exists', async () => {
+			delete (sub as any).id;
+
+			await expect(subManager.removeSubscription(sub)).rejects.toThrow(
+				'Subscription is not subscribed yet.',
+			);
+		});
+
+		it('should throw error if a subscription does not exists', async () => {
+			(sub as any).id = '456';
+
+			await expect(subManager.removeSubscription(sub)).rejects.toThrow(
+				'Subscription with id "456" does not exists',
+			);
+		});
+
+		it('should try to unsubscribe the subscription', async () => {
+			await subManager.removeSubscription(sub);
+
+			expect(sub.sendUnsubscribeRequest).toHaveBeenCalledTimes(1);
+			expect(sub.sendUnsubscribeRequest).toHaveBeenCalledWith();
+		});
+
+		it('should remove the subscription to the map', async () => {
+			expect(subManager.subscriptions).toEqual(new Map([['123', sub]]));
+
+			await subManager.removeSubscription(sub);
+
+			expect(subManager.subscriptions).toEqual(new Map());
+		});
+	});
+});
---
+++ packages/web3-utils/CHANGELOG.md
@@ -0,0 +1,184 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+<!-- EXAMPLE
+
+## [1.0.0]
+
+### Added
+
+- I've added feature XY (#1000)
+
+### Changed
+
+- I've cleaned up XY (#1000)
+
+### Deprecated
+
+- I've deprecated XY (#1000)
+
+### Removed
+
+- I've removed XY (#1000)
+
+### Fixed
+
+- I've fixed XY (#1000)
+
+### Security
+
+- I've improved the security in XY (#1000)
+
+-->
+
+## [4.0.1-alpha.1]
+
+### Added
+
+-   Added and exported three reusable utility functions: `pollTillDefined`, `rejectIfTimeout` and `rejectIfConditionAtInterval` which are useful when dealing with promises that involves polling, rejecting after timeout or rejecting if a condition was met when calling repeatably at every time intervals.
+
+## [4.0.1-alpha.2]
+
+### Added
+
+-   Export a new function `uuidV4` that generates a random v4 Uuid (#5373).
+-   Enable passing a starting number, to increment based on it, for the Json Rpc Request `id` (#5652).
+-   Export a new function `isPromise` that checks if an object is a promise (#5652).
+
+### Fixed
+
+-   Use Uuid for the response id, to fix the issue "Responses get mixed up due to conflicting payload IDs" (#5373).
+
+## [4.0.1-alpha.3]
+
+### Changed
+
+-   Updated dependencies (#5725)
+
+## [4.0.1-alpha.4]
+
+### Changed
+
+-   `tsc` compiled files moved to `lib/` directory from `dist/` (#5739)
+-   Add SocketProvider class and Eip1193Provider abstract class (#5683)
+
+## [4.0.1-alpha.5]
+
+### Changed
+
+-   web3.js dependencies (#5757)
+
+## [4.0.1-rc.0]
+
+### Removed
+
+-   Moved `TypedArray` to `web3-types` (was also duplicated at `web3-validator`) (#5771)
+-   Removed support of `genesis` tag in `compareBlockNumbers` function (#5823)
+
+### Added
+
+-   Added support of `safe` and `finalized` block tags (#5823)
+
+### Changed
+
+-   `compareBlockNumbers` function now only supports comparison of both blocktags params ( except `earliest` vs number) or both block number params (#5842)
+-   `SocketProvider` abstract class now resolves JSON RPC response errors instead of rejecting them (#5844)
+-   Exposes the getter of `SocketConnection` in `SocketProvider` (#5891)
+
+## [4.0.1-rc.1]
+
+### Added
+
+-   Added source files (#5956)
+-   Added hybrid build (ESM and CJS) of library (#5904)
+
+### Changed
+
+-   The types `FMT_NUMBER`, `NumberTypes`, `FMT_BYTES`, `ByteTypes`, `DataFormat`, `DEFAULT_RETURN_FORMAT`, `ETH_DATA_FORMAT` and `FormatType` moved to `web3-types`. (#5993)
+
+### Removed
+
+-   Removed dependencies @ethereumjs/tx, @ethereumjs/common (#5963)
+
+## [4.0.1-rc.2]
+
+### Added
+
+-   Optional `hexstrict` parameter added to numberToHex (#6004)
+
+### Changed
+
+-   Replaced Buffer for Uint8Array (#6004)
+-   The methods `hexToBytes`, `randomBytes` does not return type `Buffer` but type `Uint8Array` (#6004)
+-   The methods `sha3` and `keccak256Wrapper` does not accept type `Buffer` but type `Uint8Array` (#6004)
+-   The method `bytesToBuffer` has been removed for the usage of `bytesToUint8Array` (#6004)
+
+## [4.0.1]
+
+Release Notes:
+
+Detailed List of change logs are mentioned under previous 4.x alpha and RC releases.
+
+Documentation:
+[Web3.js documentation](https://docs.web3js.org/)
+[Web3 API](https://docs.web3js.org/api)
+[Migration Guide from 1.x](https://docs.web3js.org/guides/web3_upgrade_guide/x/)
+
+## [4.0.2]
+
+### Changed
+
+-   Dependencies updated
+
+## [4.0.3]
+
+### Fixed
+
+-   BigInts pass validation within the method `numberToHex` (#6206)
+
+## [4.0.4]
+
+### Changed
+
+-   Dependencies updated
+
+## [4.0.5]
+
+### Changed
+
+-   Dependencies updated
+
+## [4.0.6]
+
+### Fixed
+
+-   `soliditySha3()` with BigInt support
+
+## [4.0.7]
+
+### Added
+
+-   As a replacment of the node EventEmitter, a custom `EventEmitter` has been implemented and exported. (#6398)
+
+### Fixed
+
+-   Fix issue with default config with babel (and React): "TypeError: Cannot convert a BigInt value to a number #6187" (#6506)
+-   Fixed bug in chunks processing logic (#6496)
+
+## [4.1.0]
+
+### Added
+
+-   `SocketProvider` now contains public function `getPendingRequestQueueSize`, `getSentRequestsQueueSize` and `clearQueues` (#6479)
+-   Added `safeDisconnect` as a `SocketProvider` method to disconnect only when request queue size and send request queue size is 0 (#6479) 
+-   Add `isContractInitOptions` method (#6555)
+
+### Fixed
+
+-   Fix unecessary array copy when pack encoding (#6553)
+
+## [Unreleased]
